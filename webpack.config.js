863fccc7d35da2d843840f90b74a4bdb54868ac6
Oksana Astapova <astapova.frontend@gmail.com>
2022-07-14 21:40:09 +0300

    feat: add styles in project (Thu, Jul 14, 2022 09:40:06 PM)

--- webpack.config.js
+++ webpack.config.js
@@ -17,4 +17,24 @@ module.exports = {
       filename: 'index.html', // output file
     }),
   ],
+  module: {
+    rules: [
+      // JavaScript
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      // CSS, PostCSS, and Sass
+      {
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+      // Images
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
 }
